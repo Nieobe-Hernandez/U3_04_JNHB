b458d6328b7513154dacf9d485b4d290863f0755
Nieobe Hernandez <20223tn057@utez.edu.mx>
2025-06-20 11:54:15 -0600

    [UPDATE] actualización de configuraciones de seguridad

--- U3_04_JNHB/U3_04_JNHB/src/main/java/utez/edu/mx/U3_04_JNHB/Controller/AuthController.java
+++ U3_04_JNHB/U3_04_JNHB/src/main/java/utez/edu/mx/U3_04_JNHB/Controller/AuthController.java
@@ -1,39 +1,52 @@
-//package utez.edu.mx.U3_04_JNHB.Controller;
-//
-//import lombok.Data;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import utez.edu.mx.U3_04_JNHB.Security.JwtUtil;
-//import utez.edu.mx.U3_04_JNHB.Security.UserDetailsServiceImpl;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//@RequiredArgsConstructor
-//public class AuthController {
-//
-//    private final AuthenticationManager authenticationManager;
-//    private final UserDetailsServiceImpl userDetailsService;
-//    private final JwtUtil jwtUtil;
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestBody AuthRequest auth) throws Exception {
-//        UsernamePasswordAuthenticationToken authentication =
-//                new UsernamePasswordAuthenticationToken(auth.getUsername(), auth.getPassword());
-//        authenticationManager.authenticate(authentication);
-//        UserDetails user = userDetailsService.loadUserByUsername(auth.getUsername());
-//        return ResponseEntity.ok(jwtUtil.generateToken(user));
-//    }
-//}
-//
-//@Data
-//class AuthRequest {
-//    private String username;
-//    private String password;
-//}
+package utez.edu.mx.U3_04_JNHB.Controller;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import utez.edu.mx.U3_04_JNHB.Model.Usuario;
+import utez.edu.mx.U3_04_JNHB.Security.JwtUtil;
+import utez.edu.mx.U3_04_JNHB.Security.UserDetailsServiceImpl;
+import utez.edu.mx.U3_04_JNHB.Service.UsuarioService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtil jwtUtil;
+    private final UsuarioService usuarioService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest auth) {
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(auth.getUsername(), auth.getPassword());
+        authenticationManager.authenticate(authentication);
+        UserDetails user = userDetailsService.loadUserByUsername(auth.getUsername());
+        return ResponseEntity.ok(jwtUtil.generateToken(user));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Usuario> register(@RequestBody RegisterRequest usuario) {
+        Usuario nuevoUsuario = new Usuario();
+        nuevoUsuario.setUsername(usuario.getUsername());
+        nuevoUsuario.setPassword(usuario.getPassword());
+        nuevoUsuario.setRole(usuario.getRole().toUpperCase());
+        Usuario registrado = usuarioService.guardar(nuevoUsuario);
+        return ResponseEntity.ok(registrado);
+    }
+}
+
+@Data
+class AuthRequest {
+    private String username;
+    private String password;
+}
--- U3_04_JNHB/U3_04_JNHB/src/main/java/utez/edu/mx/U3_04_JNHB/Service/UsuarioService.java
+++ U3_04_JNHB/U3_04_JNHB/src/main/java/utez/edu/mx/U3_04_JNHB/Service/UsuarioService.java
@@ -1,22 +1,22 @@
-//package utez.edu.mx.U3_04_JNHB.Service;
-//
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//import utez.edu.mx.U3_04_JNHB.Model.Usuario;
-//import utez.edu.mx.U3_04_JNHB.Repository.UsuarioRepository;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class UsuarioService {
-//    private final UsuarioRepository repository;
-//
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public Usuario guardar(Usuario usuario) {
-//        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
-//        return repository.save(usuario);
-//    }
-//}
+package utez.edu.mx.U3_04_JNHB.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import utez.edu.mx.U3_04_JNHB.Model.Usuario;
+import utez.edu.mx.U3_04_JNHB.Repository.UsuarioRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UsuarioService {
+    private final UsuarioRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public Usuario guardar(Usuario usuario) {
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        return repository.save(usuario);
+    }
+}
